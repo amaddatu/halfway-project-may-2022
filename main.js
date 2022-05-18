574c8c5f3a4187b598dddd13a4133ca0c731f01f
amaddatu <>
2022-05-18 19:00:42 -0400

    Add finished style

--- main.js
+++ main.js
@@ -12,12 +12,15 @@ console.log("main");
 let spanTags = $('h1 > span');
 let count = 0;
 
-// for(var i = 0; i < spanTags.length; i++){
-//   console.log(spanTags.get(i));
-// }
+for(var i = 0; i < spanTags.length; i++){
+  let span = $(spanTags.get(i));
+  let text = span.text();
+  span.get(0).innerHTML = `<span class="gradient">${text}</span>`;
+}
 
 let interval = setInterval(() => {
-  $(spanTags.get(count)).addClass("animate__animated animate__bounce animate__infinite show");
+
+  $(spanTags.get(count)).addClass("animate__animated animate__rotateIn animate__infinite show");
 
   count++;
   if(count >= spanTags.length){
